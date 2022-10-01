29779e888a740a063c052e04a9a09c499d2fb89e
Igor Daniel da Silva <44319834+igor-daniel@users.noreply.github.com>
2022-09-30 21:49:55 -0300

    atributo alt adicionado nas imagens

---
+++ script.js
@@ -0,0 +1,8 @@
+let tittle = document.querySelector('.content-skills-desc h1');
+let description = document.querySelector('.content-skills-desc p');
+let html = document.querySelector('.content-skills-icons img[alt="html"]');
+let css = document.querySelector('.content-skills-icons img[alt="css"]');
+let js = document.querySelector('.content-skills-icons img[alt="js"]');
+let git = document.querySelector('.content-skills-icons img[alt="git"]');
+
+html.addEventListener('hover', escreve)
