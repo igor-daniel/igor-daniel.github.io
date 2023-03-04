9b3e103807b8687d111dfd22da26680917518316
Igor Daniel da Silva <44319834+igor-daniel@users.noreply.github.com>
2023-03-04 13:28:08 -0300

    footer adicionado

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import { Header } from './components/Header/Header'
 import { Main } from './components/Main/Main'
+import { Sobre } from './components/sobre/Sobre'
+import { Footer } from './components/Footer/Footer'
 
 function App() {
   return (
     <div>
       <Header />
       <Main />
+      <Sobre />
+      <Footer />
     </div>
   );
 }
